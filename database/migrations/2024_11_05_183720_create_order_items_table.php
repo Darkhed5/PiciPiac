2eeca56c58b49cd7938b21103624760fb044e709
Dobronyi Csaba <dobronyicsaba@elg-bp.edu.hu>
2024-11-05 20:06:45 +0100

    Kosár továbbfejlesztve

--- database/migrations/2024_11_05_183720_create_order_items_table.php
+++ database/migrations/2024_11_05_183720_create_order_items_table.php
@@ -4,24 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrderItemsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id'); // Rendelés ID
+            $table->unsignedBigInteger('product_id'); // Termék ID
+            $table->integer('quantity'); // Mennyiség
+            $table->decimal('price', 10, 2); // A termék ára a rendelés pillanatában
             $table->timestamps();
+
+            // Külső kulcsok
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('order_items');
     }
-};
+}
