8d153a246199fbc9b59842c310ec86835b0b8550
Dobronyi Csaba <dobronyicsaba@elg-bp.edu.hu>
2024-11-10 21:50:14 +0100

    Rendelési előzmények javítva

--- database/migrations/2024_11_01_160816_create_products_table.php
+++ database/migrations/2024_11_01_160816_create_products_table.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
@@ -16,21 +12,16 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
-            $table->string('category');
-            $table->integer('stock')->default(0);
+            $table->unsignedBigInteger('user_id'); // Kapcsolódik a felhasználóhoz
             $table->timestamps();
+
+            // Külső kulcs beállítása a felhasználóra
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
-    
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
 };
-
-
-
