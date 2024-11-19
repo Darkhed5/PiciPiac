b2e3abcec5486a3f31595f473bd3e39474a9ce9c
Dobronyi Csaba <dobronyicsaba@elg-bp.edu.hu>
2024-11-19 19:56:24 +0100

    Jelszó-visszaállítás

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,15 +8,15 @@
     <form method="GET" action="{{ url('/products') }}" class="mb-4">
         <label for="category">Kategória:</label>
         <select name="category" id="category" onchange="this.form.submit()">
-            <option value="">Minden kategória</option>
+            <option value="">Összes</option>
             <option value="gyumolcsok" {{ request('category') == 'gyumolcsok' ? 'selected' : '' }}>Gyümölcsök</option>
             <option value="zoldsegek" {{ request('category') == 'zoldsegek' ? 'selected' : '' }}>Zöldségek</option>
             <option value="tejtermekek" {{ request('category') == 'tejtermekek' ? 'selected' : '' }}>Tejtermékek</option>
-            <option value="hus-es-huskeszitmenyek" {{ request('category') == 'hus-es-huskeszitmenyek' ? 'selected' : '' }}>Hús és Húskészítmények</option>
-            <option value="kezmuves-termekek" {{ request('category') == 'kezmuves-termekek' ? 'selected' : '' }}>Kézműves Termékek</option>
-            <option value="mezek-es-lekvarok" {{ request('category') == 'mezek-es-lekvarok' ? 'selected' : '' }}>Mézek és Lekvárok</option>
+            <option value="hus-es-huskeszitmenyek" {{ request('category') == 'hus-es-huskeszitmenyek' ? 'selected' : '' }}>Hús és húskészítmények</option>
+            <option value="kezmuves-termekek" {{ request('category') == 'kezmuves-termekek' ? 'selected' : '' }}>Kézműves termékek</option>
+            <option value="mezek-es-lekvarok" {{ request('category') == 'mezek-es-lekvarok' ? 'selected' : '' }}>Mézek és lekvárok</option>
             <option value="pekaruk" {{ request('category') == 'pekaruk' ? 'selected' : '' }}>Pékáruk</option>
-            <option value="fuszerek-es-gyogynovenyek" {{ request('category') == 'fuszerek-es-gyogynovenyek' ? 'selected' : '' }}>Fűszerek és Gyógynövények</option>
+            <option value="fuszerek-es-gyogynovenyek" {{ request('category') == 'fuszerek-es-gyogynovenyek' ? 'selected' : '' }}>Fűszerek és gyógynövények</option>
         </select>
     </form>
 
@@ -34,7 +34,6 @@
                         <h5 class="card-title">{{ $product->name }}</h5>
                         <p class="card-text">{{ $product->description }}</p>
                         <p class="card-text"><strong>Ár:</strong> {{ $product->price }} Ft</p>
-                        <p class="card-text"><strong>Kategória:</strong> {{ $product->category }}</p>
                         <p class="card-text"><strong>Készlet:</strong> {{ $product->stock > 0 ? 'Raktáron' : 'Elfogyott' }}</p>
 
                         <!-- Szerkesztés és Törlés Gombok -->
