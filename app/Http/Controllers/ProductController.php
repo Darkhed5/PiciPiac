580f306a2202e79e0252b4f31d0d6b5ae8474ca6
judyguraby <guraby.judit@gmail.com>
2024-12-10 19:28:38 +0100

    javtás

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Auth;
 class ProductController extends Controller
 {
     public function index(Request $request)
-{
-    $search = $request->input('search');
-    $category = $request->input('category');
-    $minPrice = $request->input('min_price');
-    $maxPrice = $request->input('max_price');
-    $inStock = $request->boolean('in_stock');
-    $orderBy = $request->input('order_by', 'name'); // Alapértelmezett rendezés: név szerint
-    $orderDirection = $request->input('order_direction', 'asc'); // Alapértelmezett: növekvő
+    {
+        $search = $request->input('search');
+        $category = $request->input('category');
+        $minPrice = $request->input('min_price');
+        $maxPrice = $request->input('max_price');
+        $inStock = $request->boolean('in_stock');
+        $orderBy = $request->input('order_by', 'name');
+        $orderDirection = $request->input('order_direction', 'asc');
 
         $query = Product::query();
 
@@ -24,20 +24,20 @@ class ProductController extends Controller
             $query->where(function ($q) use ($search) {
                 $q->where('name', 'LIKE', "%{$search}%")
                   ->orWhere('description', 'LIKE', "%{$search}%");
-            });
+            }); // Zárójel biztosan zárva
         }
 
         if ($category) {
             $query->where('category', $category);
         }
 
-    if ($minPrice) {
-        $query->where('price', '>=', $minPrice);
-    }
+        if ($minPrice) {
+            $query->where('price', '>=', (float) $minPrice);
+        }
 
-    if ($maxPrice) {
-        $query->where('price', '<=', $maxPrice);
-    }
+        if ($maxPrice) {
+            $query->where('price', '<=', (float) $maxPrice);
+        }
 
         if ($inStock) {
             $query->where('stock', '>', 0);
@@ -50,8 +50,20 @@ class ProductController extends Controller
         // Kategóriák lekérdezése
         $categories = Product::select('category')->distinct()->get();
 
-    // Legnépszerűbb termékek lekérdezése
-    $popularProducts = Product::orderBy('views', 'desc')->take(4)->get();
+        // Népszerű termékek lekérdezése
+        $popularProducts = Product::orderBy('views', 'desc')->take(4)->get();
 
-    return view('home', compact('products', 'categories', 'popularProducts', 'search', 'category', 'minPrice', 'maxPrice', 'inStock', 'orderBy', 'orderDirection'));
-}
+        return view('home', compact(
+            'products',
+            'categories',
+            'popularProducts',
+            'search',
+            'category',
+            'minPrice',
+            'maxPrice',
+            'inStock',
+            'orderBy',
+            'orderDirection'
+        ));
+    } // <- Zárva a `index` metódus
+} // <- Zárva a `ProductController` osztály
