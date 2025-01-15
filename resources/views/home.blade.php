67beb875c0d03b3032bdd9948d117e8ff535264b
Dobronyi Csaba <dobronyicsaba@elg-bp.edu.hu>
2025-01-15 19:14:36 +0100

    home javítás

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,34 +5,12 @@
     <h1 class="text-center mt-5">Üdvözlünk a PiciPiacon!</h1>
     <p class="text-center">
         A PiciPiac egy online piactér, amely összekapcsolja a helyi termelőket és vásárlókat.
-        Fedezd fel friss, helyi termékeinket!
+        Fedezd fel friss, helyi termékeinket! 
     </p>
-
-    <!-- Kategóriák szekció -->
-    <div class="categories-section mt-5">
-        <h2 class="text-center mb-4">Kategóriák</h2>
-        <div class="row justify-content-center">
-            @if($categories->isNotEmpty())
-                @foreach($categories as $category)
-                    <div class="col-md-3 mb-3">
-                        <div class="card text-center border-success">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $category->category }}</h5>
-                                <a href="{{ route('products.index', ['category' => $category->category]) }}" class="btn btn-outline-success">Keresés</a>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            @else
-                <p class="text-center">Jelenleg nincsenek elérhető kategóriák.</p>
-            @endif
-        </div>
-    </div>
 
     <!-- Népszerű termékek -->
     <div class="popular-products-section mt-5">
         <h2 class="text-center mb-4">Termékek</h2>
-        FASZOM!!!!
         <div class="row">
             @if($popularProducts->isNotEmpty())
                 @foreach($popularProducts as $product)
